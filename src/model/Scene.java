26a2ae86e7cd184fd14e19d604c4a54a06bdebb4
Pierre Pallier <45609246+Bregamir@users.noreply.github.com>
2020-01-07 15:04:36 +0100

    Base
    Modèle comencé + packages déclarés

---
+++ src/model/Scene.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Scene {
+
+	private int id;
+	private String desc;
+	private ArrayList<Setup> listSetup;
+	private Location filmLocation;
+	
+	
+}
