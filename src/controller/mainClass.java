41c0fd9d3f2ebcd9bebab5729b331f53f47569c6
Betshet <45632828+Betshet@users.noreply.github.com>
2020-01-10 12:03:40 +0100

    fait un main + fini affichage scene

--- src/controller/mainClass.java
+++ src/controller/mainClass.java
@@ -9,18 +9,47 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import model.Clap;
+import model.FilmRoll;
 import model.IndoorScene;
 import model.Location;
 import model.OutdoorScene;
 import model.Scene;
+import model.Setup;
 
 
 public class mainClass {
 
 	public static void main(String[] args) {
 		
+		FilmRoll roll = new FilmRoll();
+		
+		ArrayList<Clap> clapList = new ArrayList<Clap>();
+		clapList.add(new Clap(1, 370, roll) );
+		clapList.add(new Clap(2, 242, roll) );
+		clapList.add( new Clap(3, 70, roll) );
+		clapList.add(new Clap(4, 55, roll));
+		clapList.add(new Clap(5, 228, roll));
+		
+		
+		ArrayList<Clap> clapList2 = new ArrayList<Clap>();
+		clapList.add(new Clap(6, 842, roll) );
+		clapList.add(new Clap(7, 37, roll) );
+		clapList.add( new Clap(8, 82, roll) );
+		clapList.add(new Clap(9, 180, roll));
+		clapList.add(new Clap(10, 372, roll));
+		
+		ArrayList<Setup> setupList = new ArrayList<Setup>();
+		setupList.add(new Setup(1, "Ceci est une description", clapList));
+		setupList.add(new Setup(2, "Ceci est une description : le retour", clapList2));
+
 		Location loc = new Location(1,"Paris","la tour eiffel wesh");
-		IndoorScene sc = new IndoorScene(/*0,"description lol"*/);
+		
+		ArrayList<Scene> scList = new ArrayList<Scene>();
+		scList.add( new OutdoorScene(1, "Lorem Ipsum", setupList, loc, 0));
+		
+		SceneWindow scWindow = new SceneWindow(scList);
+		scWindow.afficher();
 		
 		
 		//SceneWindow window = new SceneWindow();
